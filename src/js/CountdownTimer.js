e6074f524758ac98291f48581cf06dcca9398ce5
Carmen Alvarez <c@rmen.ca>
2021-04-05 16:08:58 +0200

    wip

---
+++ src/js/CountdownTimer.js
@@ -0,0 +1,35 @@
+/**
+Copyright (c) 2021 - present Carmen Alvarez
+This file is part of Scale Tutor.
+Scale Tutor is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+Scale Tutor is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with Scale Tutor.  If not, see <http://www.gnu.org/licenses/>.
+*/
+class CountdownTimer {
+    constructor(elem) {
+        this._elem = elem
+    }
+
+    start = (durationS) =>
+        new Promise(completionFunc => {
+            var remainingTimeS = durationS
+            this._elem.style.display = "block"
+            this._elem.innerText = remainingTimeS
+            const intervalId = setInterval(() => {
+                remainingTimeS--
+                this._elem.innerText = remainingTimeS
+                if (remainingTimeS == 0) {
+                    clearInterval(intervalId)
+                    this._elem.style.display = "none"
+                    completionFunc()
+                }
+            }, 1000)
+        })
+}
