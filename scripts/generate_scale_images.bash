bccea9d0f6a0f2468d3b2051b8b5de45944ae85f
Carmen Alvarez <c@rmen.ca>
2021-04-11 01:04:45 +0200

    Generate minor scales in bass clef

--- scripts/generate_scale_images.bash
+++ scripts/generate_scale_images.bash
@@ -1,6 +1,7 @@
-lilypond_major_notes=(   c des d ees e f ges g aes a bes b)
-lilypond_minor_notes=(   c\' cis\' d\' ees\' e\' f\' fis\' g\' aes\' a bes b)
-scale_tutor_notes=(      c cs  d ds  e f fs  g gs  a as  b)
+lilypond_major_notes=(     c   des   d   ees   e   f   ges   g   aes   a   bes   b)
+lilypond_minor_notes=(     c\' cis\' d\' ees\' e\' f\' fis\' g\' aes\' a   bes   b)
+lilypond_minor_bass_notes=(c\' cis\' d\' ees\' e\' f\' fis\' g\' aes\' a\' bes\' b\')
+scale_tutor_notes=(        c   cs    d   ds    e   f   fs    g   gs    a   as    b)
 
 for i in ${!lilypond_major_notes[@]}; do
   echo "${scale_tutor_notes[$i]} major"
@@ -15,6 +16,8 @@ for minor_type in natural harmonic melodic; do
     echo "${scale_tutor_notes[$i]} ${minor_type} minor"
     sed -e "s/transpose a a/transpose a ${lilypond_minor_notes[$i]}/" etc/a_${minor_type}_minor.ly > /tmp/temp.ly
     lilypond -dbackend=svg -o src/resources/${scale_tutor_notes[$i]}_${minor_type}_minor /tmp/temp.ly >/dev/null 2>&1
+    sed -e "s/transpose a a/transpose a'' ${lilypond_minor_bass_notes[$i]}/;s/\\\\key a \\\\minor/\\\\key a \\\\minor\\\\clef bass/" etc/a_${minor_type}_minor.ly > /tmp/temp.ly
+    lilypond -dbackend=svg -o src/resources/${scale_tutor_notes[$i]}_${minor_type}_minor_bass /tmp/temp.ly > /dev/null 2>&1
   done
 done
 
