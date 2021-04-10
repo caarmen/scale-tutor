3ce01c3a6874eb4af4011a85db124cf77eb8601c
Carmen Alvarez <c@rmen.ca>
2021-04-11 00:25:10 +0200

    Regenrate natural minor scales

--- scripts/generate_scale_images.bash
+++ scripts/generate_scale_images.bash
@@ -1,8 +1,13 @@
-lilypond_notes=(   c des d ees e f ges g aes a bes b)
-scale_tutor_notes=(c cs  d ds  e f fs  g gs  a as  b)
+lilypond_major_notes=(   c des d ees e f ges g aes a bes b)
+lilypond_minor_notes=(   c\' cis\' d\' dis\' e\' f\' fis\' g\' gis\' a bes b)
+scale_tutor_notes=(      c cs  d ds  e f fs  g gs  a as  b)
 
-for i in ${!lilypond_notes[@]}; do
-  #echo "${lilypond_notes[$i]} ${scale_tutor_notes[$i]}"
-  sed -e "s/transpose c c/transpose c ${lilypond_notes[$i]}/" etc/c_major.ly > /tmp/temp.ly
+for i in ${!lilypond_major_notes[@]}; do
+  sed -e "s/transpose c c/transpose c ${lilypond_major_notes[$i]}/" etc/c_major.ly > /tmp/temp.ly
   lilypond -dbackend=svg -o src/resources/${scale_tutor_notes[$i]}_major /tmp/temp.ly
+done
+
+for i in ${!lilypond_minor_notes[@]}; do
+  sed -e "s/transpose a a/transpose a ${lilypond_minor_notes[$i]}/" etc/a_natural_minor.ly > /tmp/temp.ly
+  lilypond -dbackend=svg -o src/resources/${scale_tutor_notes[$i]}_natural_minor /tmp/temp.ly
 done
