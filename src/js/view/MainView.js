a4f0ce4aa3c825f272cf543fea59a10b1b4c2d88
Carmen Alvarez <c@rmen.ca>
2021-04-11 10:16:31 +0200

    more templates

--- src/js/view/MainView.js
+++ src/js/view/MainView.js
@@ -20,8 +20,10 @@ class MainView {
         this._scaleView = new ScaleView()
         this._controlsView = new ControlsView(this._viewModel.i18n)
 
-        this._mdcDialog = new mdc.dialog.MDCDialog(document.querySelector('#settings-dialog'))
-        this._elemNoteNamesValueLabel = document.querySelector("#setting__note-names")
+        this._mdcDialog
+
+        this._elemSettingNoteNamesFormatLabel
+        this._elemSettingNoteNamesFormatValue
         this._initViews()
         this._bindViewModel()
     }
@@ -29,16 +31,18 @@ class MainView {
     _initViews() {
         this._controlsView.initViews()
         this._elemButtonSettings.onclick = () => this._showSettings()
-        document.querySelectorAll(".template-dialog-ok-button").forEach((element) => {
-            element.innerHTML = templateDialogOkButton
-        })
+        document.querySelector("#placeholder-settings-dialog").innerHTML = templateDialog("settings-dialog", "settings_title", templateSettings, "")
         document.querySelector("#placeholder-setting__tts-enabled").innerHTML = templateToggle("setting__tts-enabled", "setting_tts_enabled")
         document.querySelector("#placeholder-setting__autoplay-enabled").innerHTML = templateToggle("setting__autoplay-enabled", "setting_autoplay_enabled")
-        document.querySelector("#placeholder-options-note-name-format").innerHTML =
-            templateDialog("options-note-name-format", "setting_title_note_names", templateOptionsNoteNameFormat, "settings-detail-dialog")
+        document.querySelector("#placeholder-setting__note-names").innerHTML = templateSetting("note-names", "setting_title_note_names")
+        document.querySelector("#placeholder-options-note-name-format").innerHTML = templateDialog("options-note-name-format", "setting_title_note_names", templateOptionsNoteNameFormat, "settings-detail-dialog")
         document.querySelector("#placeholder_note-name-format__abc").innerHTML = templateRadio("note-name-format", "note-name-format__abc", "setting_value_note_names_abc")
         document.querySelector("#placeholder_note-name-format__solfege").innerHTML = templateRadio("note-name-format", "note-name-format__solfege", "setting_value_note_names_solfege")
+        this._elemSettingNoteNamesFormatLabel = document.querySelector("#label_setting__note-names")
+        this._elemSettingNoteNamesFormatValue = document.querySelector("#setting__note-names")
         this._viewModel.i18n.translateElement(document.documentElement)
+
+        this._mdcDialog = new mdc.dialog.MDCDialog(document.querySelector('#settings-dialog'))
     }
 
     _bindViewModel() {
@@ -65,21 +69,24 @@ class MainView {
             mdcSwitchAutoPlayEnabled.listen("change", (e) => {
                 this._viewModel.setAutoPlayEnabled(mdcSwitchAutoPlayEnabled.checked)
             })
-            this._elemNoteNamesValueLabel.innerText = this._viewModel.getNoteNamesDisplayValue()
-            this._elemNoteNamesValueLabel.onclick = () => {
-                const dialog = new mdc.dialog.MDCDialog(document.querySelector("#options-note-name-format"))
-                dialog.open()
-                const noteNameFormat = this._viewModel.getNoteNameFormat()
-                this._checkRadio("note-name-format__abc", noteNameFormat == Settings.NoteNameFormat.ABC, () => {
-                    this._viewModel.setNoteNameFormat(Settings.NoteNameFormat.ABC)
-                    this._elemNoteNamesValueLabel.innerText = this._viewModel.getNoteNamesDisplayValue()
-                })
-                this._checkRadio("note-name-format__solfege", noteNameFormat == Settings.NoteNameFormat.SOLFEGE, () => {
-                    this._viewModel.setNoteNameFormat(Settings.NoteNameFormat.SOLFEGE)
-                    this._elemNoteNamesValueLabel.innerText = this._viewModel.getNoteNamesDisplayValue()
-                })
-            }
+            this._elemSettingNoteNamesFormatValue.innerText = this._viewModel.getNoteNamesDisplayValue()
+            this._elemSettingNoteNamesFormatLabel.onclick = () => this._displayNoteNamesFormatSetting()
+            this._elemSettingNoteNamesFormatValue.onclick = () => this._displayNoteNamesFormatSetting()
         })
+    }
+    _displayNoteNamesFormatSetting() {
+        const dialog = new mdc.dialog.MDCDialog(document.querySelector("#options-note-name-format"))
+        dialog.open()
+        const noteNameFormat = this._viewModel.getNoteNameFormat()
+        this._checkRadio("note-name-format__abc", noteNameFormat == Settings.NoteNameFormat.ABC, () => {
+            this._viewModel.setNoteNameFormat(Settings.NoteNameFormat.ABC)
+            this._elemSettingNoteNamesFormatValue.innerText = this._viewModel.getNoteNamesDisplayValue()
+        })
+        this._checkRadio("note-name-format__solfege", noteNameFormat == Settings.NoteNameFormat.SOLFEGE, () => {
+            this._viewModel.setNoteNameFormat(Settings.NoteNameFormat.SOLFEGE)
+            this._elemSettingNoteNamesFormatValue.innerText = this._viewModel.getNoteNamesDisplayValue()
+        })
+
     }
     _checkRadio(id, checked, listener) {
         const radioControl = new mdc.radio.MDCRadio(document.querySelector(`#${id}__mdc-radio`))
